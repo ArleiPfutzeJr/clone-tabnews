a60b0e6792574660a9bc19986f6ea62197870eb6
ArleiPfutzeJr <https://github.com/ArleiPfutzeJr>
2025-02-25 19:51:45 -0300

    feat: add prettier on project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# clone-tabnews
+Implementação do https://www.tabnews.com.br para https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-function Home(){
-  return <h1>
-    Khianny, eu te amo, voce e o gordinho!!
-  </h1>
+function Home() {
+  return <h1>Khianny, eu te amo, voce e o gordinho!!</h1>;
 }
 
 export default Home;
